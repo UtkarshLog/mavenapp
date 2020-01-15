049cd685ae56b9b333c9031fae8ac73ab6d2afd8
Utkarsh <urastogi94@gmail.com>
2020-01-15 09:48:45 +0000

    new change in newfile.c

--- newfile.c
+++ newfile.c
@@ -1,1 +1,21 @@
-bin/bash: q: command not found
+#include<stdio.h>
+int a = 10
+printf("This is a new program in the same file");
+ER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT                                                           
+ec2-user pts/0    ec2-13-233-177-0 09:37    1.00s  0.16s  0.00s /bin/bash -c (w) < /tmp/vb1Ujyc/2 >                                                           
+/tmp/vb1Ujyc/3 2>&1                                                          
+USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+ec2-user pts/0    ec2-13-233-177-0 09:37    1.00s  0.16s  0.00s /bin/bash -c (w) < /tmp/vb1Ujyc/2 >/tmp/vb1Ujyc/3 2>&1
+ec2-user pts/1    ec2-13-233-177-1 07:43    1:59m  0.02s  0.02s -bash
+ec2-user pts/2    ec2-13-233-177-1 09:24   19:13   0.01s  0.01s -basER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT                                                                
+                                                           
+ec2-user pts/0    ec2-13-233-177-0 09:37    1.00s  0.16s  0.00s /bin/bash -c (w) < /tmp/vb1Ujyc/2 >                                                           
+                                                           
+/tmp/vb1Ujyc/3 2>&1                                                           
+USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT                                                           
+ec2-user pts/0    ec2-13-233-177-0 09:37    1.00s  0.16s  0.00s /bin/bash -c (w) < /tmp/vb1Ujyc/2 >                                                           
+/tmp/vb1Ujyc/3 2>&1                                                           
+ec2-user pts/1    ec2-13-233-177-1 07:43    1:59m  0.02s  0.02s -bash                                                           
+ec2-user pts/2    ec2-13-233-177-1 09:24   19:13   0.01s  0.01s -bas                                                           
+~                                                                                                                                                             
+~                                     
